76a366c2f6ffe3a3902f470bbee05c2b3dc8cb20
kvddtw7 <koen.vanderlinden@colruytgroup.com>
2024-03-15 15:38:24 +0100

    2nd attempt for github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,12 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideAnimationsAsync(),
+  ],
 };
